2a810bdf3f4cf0b688b059a7c0920e3703af47a1
28010-55-08 <28010-55-08@exchange.ad.afpanet>
2022-11-08 09:28:06 +0100

    First commit

---
+++ src/Controller/PagesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PagesController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_home")
+     */
+    public function home(): Response
+    {
+        /* dd(date_default_timezone_get()); */ /* Permet de dump la valeur du timezone */
+
+        return $this->render('pages/home.html.twig');
+    }
+}
